e776469b7468f0a40389dd004d1ce3be4a18d24f
Koji Shiraishi <shiraco@gmail.com>
2015-02-17 08:27:32 +0900

    Apply render_template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug import secure_filename
 
 # UPLOAD_FOLDER = '/var/www/uploads/'
@@ -24,15 +24,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('index.html')
 
 
 @app.route('/uploaded_file')
